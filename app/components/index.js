e89aba5b502aec70095f75d870faaa63c0898574
Jurek Barth <post@jurekbarth.de>
2016-05-02 14:47:39 +0200

    initial commit

---
+++ app/components/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class IndexComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Homepage</p>
+      </div>
+    );
+  }
+}
